d6055c7e6aa41d4de6744134eae3158186c6731a
united-forever <62713090+united-forever@users.noreply.github.com>
2020-04-05 02:19:12 +0530

    Update nodeMongoConnect.js

--- nodeMongoConnect.js
+++ nodeMongoConnect.js
@@ -3,7 +3,7 @@ const {MongoClient} = require('mongodb');
 // Connect to Mongodb server
 async function main(){
 
-const uri = "mongodb+srv://yash_verma:161195@jspsych-eymdu.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://yash_verma:*****@jspsych-eymdu.mongodb.net/test_db?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 try{
